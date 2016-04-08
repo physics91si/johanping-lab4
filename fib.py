62c71a7acea1e18a698bb13093ec9bb8e329c79c
Hannah O'Day <odayj@DN51o83n.SUNet>
2016-04-07 23:14:18 -0700

    Here is modified fib.py for submission lab4

--- fib.py
+++ fib.py
@@ -25,14 +25,15 @@ Usage
   - './fib.py help': display this help message.
 
 """
-        print(help_message)
+        print help_message
     elif args[0] == "approx" and len(args) == 2:
         phi_approx(int(args[1]))
     elif args[0] == "converge" and len(args) == 1:
         phi_converge()
     else:
-        print("Error: input not understood.\n" \
-                "    Type './fib.py help' for info on this program.")
+        print "Error: input not understood.\n" \
+                "    Type './fib.py help' for info on this program."
+
 def fib(n):
     """Return nth element of the Fibonacci sequence."""
     # Create the base case
@@ -54,19 +55,19 @@ phi_approx_output_format = \
 
 def phi_approx(n, show_output=True):
     """Return the nth-order Fibonacci approximation to the golden ratio."""
-    fib_n = fibbb(n)
+    fib_n = fib(n)
     fib_nm1 = fib(n - 1)
     phi = float(fib_n)/fib_nm1
     if show_output:
-        print(phi_approx_output_format.format(n, fib_n, fib_nm1, phi))
+        print phi_approx_output_format.format(n, fib_n, fib_nm1, phi)
     return phi
 
-phi_converge_output_format = \
+phi_converge_output_format >  \
 """Approximation order: {:d}
     phi_old: {:.25f}
     phi_new: {:.25f}"""
 
-def phi_converge():
+def phi_converge(filename):
     """Keep calculating higher-order Fibonacci approximations to the golden
     ratio until it stops changing (to floating-point precision)."""
 
@@ -77,6 +78,7 @@ def phi_converge():
         i += 1
         phi_old = phi_new
         phi_new = phi_approx(i, show_output=False)
-        print(phi_converge_output_format.format(i, phi_new, phi_old))
-    print("\nConverged to %.25f" % phi_new)
+      	phi_converge_output_format.format(i,phi_new,phi_old) > filename
+    	print "\nConverged to %.25f" % phi_new
+
 if __name__ == '__main__': main()
